7d6bb6ccb9601650b3d18a1103f911f575c2f238
Edgar Piera <edgar.piera@neogrup.com>
2019-11-15 11:54:58 +0100

    - Update version

--- nc-items-grid.js
+++ nc-items-grid.js
@@ -91,7 +91,7 @@ class NcItemsGrid extends mixinBehaviors([AppLocalizeBehavior], MutableData(Poly
 
         .justified {
           @apply --layout-center-justified;
-          margin-left: 0px
+          margin-left: 0px;
         }
 
         .loading{
